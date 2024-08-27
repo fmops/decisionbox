3c53849fe09030f7491709f1d2583e5b152ef580
Feynman Liang <feynman@blueteam.ai>
2024-08-27 12:10:36 -0700

    Separate structured response

--- examples/instructor_bool.py
+++ examples/instructor_bool.py
@@ -8,46 +8,14 @@ client = from_openai(OpenAI(), mode=Mode.TOOLS_STRICT)
 class Decision(BaseModel):
     value: bool
 
-#
-# resp = client.chat.completions.create(
-#     response_model=Decision,
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "I need vitamin C. Should I eat pears?",
-#         }
-#     ],
-#     model="gpt-4o",
-# )
-# print(resp)
-
-# TODO: call excision
-import asyncio
-import aiohttp
-
-EXBOX_URL = "http://localhost:4000/api/decision_sites/1/invoke"
-
-async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.post(EXBOX_URL, json={
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "I need vitamin C. Should I eat pears?",
-                }
-            ],
-            "model": "gpt-4o-mini",
-            "response_format": {
-                "type": "json_schema",
-                "json_schema": {
-                    "name": "decision",
-                    "schema": {
-                        "type": "boolean"
-                    }
-                }
-            }
-        }) as resp:
-            print(resp)
-            #print(await resp.text())
-
-asyncio.run(main())
+resp = client.chat.completions.create(
+    response_model=Decision,
+    messages=[
+        {
+            "role": "user",
+            "content": "I need vitamin C. Should I eat pears?",
+        }
+    ],
+    model="gpt-4o",
+)
+print(resp)
---
+++ examples/structured_response.py
@@ -0,0 +1,36 @@
+import os
+
+import asyncio
+import aiohttp
+
+EXBOX_URL = "http://localhost:4000/api/decision_sites/1/invoke"
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.post(EXBOX_URL, json={
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "I need vitamin C. Should I eat pears?",
+                }
+            ],
+            "model": "gpt-4o-mini",
+            # TODO: currently this is assumed by backend, allow users to specify response format
+            # "response_format": {
+            #     "type": "json_schema",
+            #     "json_schema": {
+            #         "name": "Decision",
+            #         "schema": {
+            #             "type": "object",
+            #             "properties": {
+            #                 "value": {
+            #                     "type": "boolean"
+            #                 }
+            #             },
+            #         }
+            #     }
+            # }
+        }, headers={ 'Authorization': f"Bearer {os.environ.get('OPENAI_API_KEY')}" }) as resp:
+            print(await resp.json())
+
+asyncio.run(main())
