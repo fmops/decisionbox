c341841046914b23e86023f3cbad91353622a332
Feynman Liang <feynman@blueteam.ai>
2024-08-28 08:25:12 -0700

    Working train job

--- lib/excision/workers/train_classifier.ex
+++ lib/excision/workers/train_classifier.ex
@@ -3,46 +3,20 @@ defmodule Excision.Workers.TrainClassifier do
 
   alias Excision.Excisions
 
+  def test(id) do
+    perform(%Oban.Job{args: %{"classifier_id" => id}})
+  end
+  
+
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{"classifier_id" => classifier_id} = args}) do
+  def perform(%Oban.Job{args: %{"classifier_id" => classifier_id}}) do
     classifier = Excisions.get_classifier!(classifier_id, preloads: [:decision_site])
     Excisions.update_classifier(classifier, %{status: :training})
 
-    {:ok, spec} =
-      Bumblebee.load_spec({:hf, "distilbert/distilbert-base-uncased"},
-        architecture: :for_sequence_classification
-      )
-
-    # TODO: support multi-class when we can customize choices
-    spec = Bumblebee.configure(spec, num_labels: 2)
-    {:ok, model} = Bumblebee.load_model({:hf, "distilbert/distilbert-base-uncased"}, spec: spec)
-    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "distilbert/distilbert-base-uncased"})
-
-    # load data as stream
-    batch_size = 1
-    sequence_length = 64
-
-    tokenizer = Bumblebee.configure(tokenizer, length: sequence_length)
-
-    label_map = %{true => 1, false => 0}
-    df = Excisions.list_labelled_decisions_for_site(classifier.decision_site)
-      |> Enum.map(fn %Excisions.Decision{
-        input: input,
-        label: label,
-      } -> %{ label: Map.get(label_map, label), text: to_string(input)} end)
-      |> Explorer.DataFrame.new()
-    train_data = df["text"]
-      |> Explorer.Series.to_enum()
-      |> Stream.zip(Explorer.Series.to_enum(df["label"]))
-      |> Stream.chunk_every(batch_size)
-      |> Stream.map(fn batch -> 
-        {text, labels} = Enum.unzip(batch)
-        tokenized = Bumblebee.apply_tokenizer(tokenizer, text)
-        {tokenized, Nx.stack(labels)}
-      end)
+    {:ok, {%{model: model, params: params}, tokenizer}} = load_model_and_tokenizer("distilbert/distilbert-base-uncased")
+    train_data = load_data(classifier.decision_site, tokenizer)
 
     # run the fine-time
-    %{model: model, params: params} = model
     logits_model = Axon.nx(model, & &1.logits)
     loss =
       &Axon.Losses.categorical_cross_entropy(&1, &2,
@@ -58,41 +32,91 @@ defmodule Excision.Workers.TrainClassifier do
     # TODO: callback to report progress back to postgres
     # TODO: configurable number of epochs
     checkpoint_path = "checkpoints/#{classifier.id}/"
-    IO.inspect("BEFORE TRAIN")
-    trained_model_state = (
+
+    # preserve the complete loop struct for grabbing loss traces and metrics
+    # https://elixirforum.com/t/how-do-i-get-a-history-of-the-loss-from-axon/56008/7
+    loop = (
       logits_model
         |> Axon.Loop.trainer(loss, optimizer, log: 1)
         |> Axon.Loop.metric(accuracy, "accuracy")
         |> Axon.Loop.checkpoint(event: :epoch_completed, path: checkpoint_path)
+        |> then(fn loop -> %{loop | output_transform: & &1} end)
         |> Axon.Loop.run(train_data, params, epochs: 3, compiler: EXLA, strict?: false)
     )
-
-    # TODO: train test split and validate on test set
+    trained_model_state = loop.step_state.model_state
     test_results = logits_model
       |> Axon.Loop.evaluator()
       |> Axon.Loop.metric(accuracy, "accuracy")
       |> Axon.Loop.run(train_data, trained_model_state, epochs: 1, compiler: EXLA)
 
-    IO.inspect("AFTERTRAIN")
-    IO.inspect(test_results)
     Excisions.update_classifier(classifier, %{
       status: :trained,
       checkpoint_path: checkpoint_path,
-      #accuracy_train: trained_model_state.metrics["accuracy"]
+      train_accuracy: test_results[0]["accuracy"] |> Nx.to_number(),
     })
-    |> IO.inspect()
-
-    Axon.predict(model, params, "I need oranges")
-    |> IO.inspect()
-    {model, params} = [checkpoint_path, "checkpoint_2_2.ckpt"] 
-      |> Path.join() 
-      |> File.read!() 
-      |> Axon.Loop.from_state()
-    Axon.predict(model, params, "I need oranges")
-    |> IO.inspect()
-
 
     :ok
+
+
+    # example of running inference
+    Axon.predict(model, params, Bumblebee.apply_tokenizer(tokenizer, "I need oranges"))
+    |> IO.inspect()
+
+    # example of loading a checkpoint from disk
+    {:ok, spec} =
+      Bumblebee.load_spec({:hf, "distilbert/distilbert-base-uncased"},
+        architecture: :for_sequence_classification
+      )
+    model = Bumblebee.build_model(spec)
+    params = [checkpoint_path, "checkpoint_2_2.ckpt"] 
+      |> Path.join() 
+      |> File.read!() 
+      |> Axon.Loop.deserialize_state()
+      |> then(& &1.step_state.model_state)
+    Axon.predict(model, params, Bumblebee.apply_tokenizer(tokenizer, "I need oranges")) |> IO.inspect()
+    :ok
+  end
+  
+  defp load_model_and_tokenizer(model_name) do
+    {:ok, spec} =
+      Bumblebee.load_spec({:hf, model_name},
+        architecture: :for_sequence_classification
+      )
+
+    # TODO: support multi-class when we can customize choices
+    spec = Bumblebee.configure(spec, num_labels: 2)
+
+    {:ok, model} = Bumblebee.load_model({:hf, model_name}, spec: spec)
+    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, model_name})
+
+    sequence_length = 64
+    tokenizer = Bumblebee.configure(tokenizer, length: sequence_length)
+    
+    {:ok, {model, tokenizer}}
+  end
+
+  defp load_data(decision_site, tokenizer) do
+    batch_size = 64
+    label_map = %{true => 1, false => 0}
+
+    df = Excisions.list_labelled_decisions_for_site(decision_site)
+      |> Enum.map(fn %Excisions.Decision{
+        input: input,
+        label: label,
+      } -> %{ label: Map.get(label_map, label), text: to_string(input)} end)
+      |> Explorer.DataFrame.new()
+
+    # TODO: train test split and validate on test set
+    train_data = df["text"]
+      |> Explorer.Series.to_enum()
+      |> Stream.zip(Explorer.Series.to_enum(df["label"]))
+      |> Stream.chunk_every(batch_size)
+      |> Stream.map(fn batch -> 
+        {text, labels} = Enum.unzip(batch)
+        tokenized = Bumblebee.apply_tokenizer(tokenizer, text)
+        {tokenized, Nx.stack(labels)}
+      end)
+    train_data
   end
 end
 
