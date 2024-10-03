0b317dc4bdc5f9684daf26f7f035b5e545fc214b
Feynman Liang <feynman@blueteam.ai>
2024-10-02 17:56:36 -0700

    Training UX clarifications

--- lib/excision_web/live/classifier_live/index.ex
+++ lib/excision_web/live/classifier_live/index.ex
@@ -13,6 +13,11 @@ defmodule ExcisionWeb.ClassifierLive.Index do
      socket
      |> stream(:classifiers, Excisions.list_classifiers_for_decision_site(decision_site))
      |> assign(:decision_site, decision_site)
+     |> assign(
+       :enough_labels_to_train,
+       Enum.count(decision_site.decisions |> Enum.filter(&(not is_nil(&1.label_id)))) >
+         1.0 / (1.0 - frac_train())
+     )
      |> apply_action(socket.assigns.live_action, params)}
   end
 
@@ -53,35 +58,5 @@ defmodule ExcisionWeb.ClassifierLive.Index do
     {:ok, _} = Excisions.delete_classifier(classifier)
 
     {:noreply, stream_delete(socket, :classifiers, classifier)}
-  end
-
-  @impl true
-  def handle_event("promote", %{"id" => id}, socket) do
-    classifier = Excisions.get_classifier!(id)
-
-    case Excisions.promote_classifier(classifier) do
-      {:ok, _} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Classifier promoted successfully")}
-
-      {:error, %Ecto.Changeset{}} ->
-        {:noreply, socket |> put_flash(:error, "Error promoting classifier")}
-    end
-  end
-
-  @impl true
-  def handle_event("train", %{"id" => id}, socket) do
-    classifier = Excisions.get_classifier!(id)
-
-    case Excisions.train_classifier(classifier) do
-      {:ok, _} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Training job submitted successfully")}
-
-      {:error, %Ecto.Changeset{}} ->
-        {:noreply, socket |> put_flash(:error, "Error submitting training job")}
-    end
   end
 end
