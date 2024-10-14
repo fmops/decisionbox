214d6437335f90497831f4378dace27827080615
niole <niolenelson@gmail.com>
2024-10-14 07:50:47 -0700

    Update comment

--- priv/repo/migrations/20240929163620_add_classifier_model_name.exs
+++ priv/repo/migrations/20240929163620_add_classifier_model_name.exs
@@ -3,6 +3,8 @@ defmodule Excision.Repo.Migrations.AddClassifierModelName do
 
   def change do
     # add nullable column base_model_name
+    # if base_model_name not provided, distilbert/distilbert-base-uncased is set
+    # app may not work as expected until you run the migration
     alter table(:classifiers) do
       add :base_model_name, :string, null: true, default: "distilbert/distilbert-base-uncased"
     end
