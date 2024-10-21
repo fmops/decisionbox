bd5d77a2f25c2fa0e5c33145f3bb2cbdbb9841a2
Feynman Liang <feynman@blueteam.ai>
2024-10-21 16:29:55 -0700

    Default PHX_HOST to blueteam.ai instance

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,7 +52,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "decisionbox.blueteam.ai"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :excision, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
